c08a1b1a046c0967dd63f05b924d746441879a85
Joel <joelmastroiaco@gmail.com>
2026-03-16 12:24:28 -0300

    ejercicios 6 y 7 completados

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+  if (times < 0) {
+    return "ERROR";
+  }
+  let arr = new Array(times).fill(word);
+  return arr.join("");
 };
 
 // Do not edit below this line
